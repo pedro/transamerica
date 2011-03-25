895e83fea751b68458d1c25ee84bd3264ee2bb40
Pedro Belo <pedro@heroku.com>
2011-03-24 22:59:35 -0700

    added a random bot

---
+++ bots/random.rb
@@ -0,0 +1,26 @@
+class RandomBot
+  def position_hq(board, objectives)
+    begin
+      place = pick_random(board)
+    end while place.has_hq?
+    @path = [place.pos]
+    puts "hq at #{@path.inspect}"
+    @path.first
+  end
+
+  def play(board, objectives)
+    @path.sort_by { rand }.each do |pos|
+      directions_to_try = Transamerica::Board::DIRECTIONS.keys - board.adjacency(pos)
+      directions_to_try.sort_by { rand }.each do |dir|
+        move = pos + [dir]
+        next unless new_place = board.at_adjusted(move)
+        @path << new_place.pos
+        puts "going for #{move.inspect}"
+        return [move]
+      end
+    end
+  end
+  def pick_random(board)
+    board.at(rand(board.cols), rand(board.rows))
+  end
+end
